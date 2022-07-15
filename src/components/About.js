cf3d5323bde49929177bfab6ca775522a57bfda1
Edsey omenda <omendajames0317@gmail.com>
2022-07-15 11:21:15 +0300

    Creates the About component that renders the blog logo image

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function About({imgSrc="https://via.placeholder.com/215"}){
+
+    return(
+
+     
+        <aside>
+            <img src={imgSrc} alt="blog logo" />
+        </aside>
+       
+
+    )
+}
+
+export default About;
