dc145afbce7b3e75929e59bb3cf996d7b4034278
Edsey omenda <omendajames0317@gmail.com>
2022-07-15 11:02:22 +0300

    creates the header component for rendering the heading

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header(props){
+
+    console.log()
+return(
+    <div id="#head">
+       <h1>{props.name}</h1>
+    </div>
+
+)
+
+}
+
+export default Header;
